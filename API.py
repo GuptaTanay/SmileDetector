f0190b14c3b7427b54b37f705e9b5bcf2548f9b3
Prem Kumar <kprem193@gmail.com>
2018-07-31 07:18:53 +0530

    Update API.py

--- API.py
+++ API.py
@@ -1,10 +1,10 @@
 import requests
 import pandas as pd
-subscription_key = 'd7261136e76c48dd919d11bda993e08e'
+subscription_key = 'Enter you key here'
 assert subscription_key
 emotion_recognition_url = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize"
 headers  = {'Ocp-Apim-Subscription-Key': subscription_key, "Content-Type": "application/octet-stream" }
-image_path = "IMG_4025.JPG"
+image_path = "Enter your file location here"
 image_data = open(image_path, "rb").read()
 response = requests.post(emotion_recognition_url, headers=headers, data=image_data)
 response.raise_for_status()
@@ -17,7 +17,9 @@ for row in analysis:
     sum+=row['scores']['happiness']
     count+=1
 avg_happiness=(sum)/count
+#Location of file.txt
 f=open("C:/wamp64/www/Smile/file.txt","w")
+#Location of file1.txt
 f1=open("C:/wamp64/www/Smile/file1.txt","w")
 f.write(str(avg_happiness))
 f1.write(str(count))
